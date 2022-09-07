fe058f81b40ac1ce6ec60f1d5ebf5c335659852a
Nicolas Robert Nemeth <56492973+nicolasrnemeth@users.noreply.github.com>
2022-09-07 21:44:17 +0200

    Create program_#0.cpp
    code snippet for video #0

---
+++ program_#0.cpp
@@ -0,0 +1,11 @@
+// - FUNDAMENTAL CONCEPTS of PROGRAMMING
+// - with hands-on examples in C++ (ISO)
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+
+    return 0;
+}
